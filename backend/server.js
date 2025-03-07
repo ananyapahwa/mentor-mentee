856fd2e2dd198ec4bf1727c368db719a1727446c
ananyapahwa <ananyapahwa82@gmail.com>
2025-03-08 00:06:49 +0530

    cors initilaized

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const cors = require('cors');
 
 connectDB();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If you're using cookies or auth headers
+  }));
 
 
 const apiRoutes = require('./routes/api');
